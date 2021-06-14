3482728ee29c106d8f7601bec95eece793e172d0
Afonso Vieira <72760509+Ampv2002@users.noreply.github.com>
2021-06-14 19:49:16 +0100

    Add files via upload

--- Projeto.cpp
+++ Projeto.cpp
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<stdio.h>
 using namespace std;
-
+int qtd;
 class produto{
 	private:
 		float preco;
@@ -31,58 +31,98 @@ class produto{
 		}
 };
 
-class troco{
-	private:
-		float ListaMoedas[8] = {2,1,0.5,0.20,0.10,0.05,0.02,0.01};
-		int arrSize = sizeof(ListaMoedas)/sizeof(ListaMoedas[0]);
-		int QtdMoedas[arrSize] = {5,5,5,5,5,5,5,5};
-		int RetornaMoedas[arrSize] = {0,0,0,0,0,0,0,0};
+class troco_class{
 	public:
+		float ListaMoedas[8] = {2,1,0.5,0.20,0.10,0.05,0.02,0.01};
+		int arrSize = 8;
+		int QtdMoedas[8] = {5,5,5,5,5,5,5,5};
+		int RetornaMoedas[8] = {0,0,0,0,0,0,0,0};
 };
 
-void stock (int a){
+void stock (int a,int o){
+	int quantidade, escolha, escolha2;
 	int stock_inicial = 2;
 	float preco_introduzido;
+	int isStock;
+	isStock = o;
 	produto p[a];
-	for (int k = 1; k <= a; k++){
-		p[k].setposicao(k);
-		cout << "Nome do produto numero " << k << ": ";
-		cin >> p[k].nome;
-		p[k].setquantidade(stock_inicial);
-		cout << "Defina o preco: ";
-		cin >> preco_introduzido;
-		p[k].setpreco(preco_introduzido);
-	}
-	cout << "\t LISTA DE PRODUTOS";
-	for (int i = 1; i <= a; i++){
-		cout << "Nome do produto " << i << ": " << p[i].nome << "\n";
-		cout << "Preco do produto " << i << ": " << p[i].getpreco() << "\n";
-		cout << "Posicao: " << p[i].getposicao() << "\n";
-		cout << "Stock do produto" << i << ": " << p[i].getquantidade() <<"\n";
-	}
-}
-
-void troco(float troco){
-	float ListaMoedas[8] = {2,1,0.5,0.20,0.10,0.05,0.02,0.01};
-	int arrSize = sizeof(ListaMoedas)/sizeof(ListaMoedas[0]);
-	int QtdMoedas[arrSize] = {5,5,5,5,5,5,5,5};
-	int RetornaMoedas[arrSize] = {0,0,0,0,0,0,0,0};
-	for (int i = 0; i < arrSize ; i++) {
-		if (QtdMoedas[i] > 0){
-			while (troco - ListaMoedas[i] >= 0){
-				troco = troco - ListaMoedas[i];
-				QtdMoedas[i] = QtdMoedas[i] - 1
-				RetornaMoedas[i] = RetornaMoedas[i] + 1
-	  		}
+	if(isStock==0){
+		for (int k = 1; k <= a; k++){
+				p[k].setposicao(k);
+				cout << "Nome do produto numero " << k << ": ";
+				cin >> p[k].nome;
+				p[k].setquantidade(stock_inicial);
+				cout << "Defina o preco: ";
+				cin >> preco_introduzido;
+				p[k].setpreco(preco_introduzido);
 		}
 	}
+	system("cls");
+	escolha = 1000000000;
+	while (escolha != 0){
+			cout << "\t LISTA DE PRODUTOS \t\n\n";
+			for (int i = 1; i <= a; i++){
+				cout << "Nome do produto " << i << ": " << p[i].nome << "\n";
+				cout << "Preco do produto " << i << ": " << p[i].getpreco() << "\n";
+				cout << "Posicao: " << p[i].getposicao() << "\n";
+				cout << "Stock do produto" << i << ": " << p[i].getquantidade() <<"\n";
+				cout << "\n";
+			}
+			cout << "Insire a sua escolha (Para sair, prima 0):";
+			cin >> escolha; 
+			while (escolha > a || escolha < 0){
+				cout << "Opcao invalida, insire outra: ";
+				cin >> escolha;
+			}
+		system("cls");
+		if(escolha > 0){
+			cout << "Posicao: " << p[escolha].getposicao() << "\n";
+			cout << "Nome do produto: " << p[escolha].nome << "\n";
+			cout << "Preco do produto: " << p[escolha].getpreco() << "\n";
+			cout << "Quantidade: " << p[escolha].getquantidade() << "\n";
+			cout << "Deseja comprar produto?\n";
+			cout << "1- Sim\n";
+			cout << "2- Nao\n"; 
+			cout << "Insira: ";
+			cin >> escolha2;
+			system("cls");
+			if (escolha2 == 1){
+				if (p[escolha].getquantidade() == 0){
+					cout << "Produto indisponível";
+				}
+				else{
+					float dinheiro_introdutizo, troco;
+					cout << "Produto Disponivel!\n\n";
+					cout << "Quantidade inserida: ";
+					cin >> dinheiro_introdutizo;
+					troco = dinheiro_introdutizo - p[escolha].getpreco();
+					if (troco > 0){
+						troco_class t1;
+						for (int i = 0; i < t1.arrSize ; i++) {
+							if (t1.QtdMoedas[i] > 0){
+								while (troco - t1.ListaMoedas[i] >= 0){
+									troco = troco - t1.ListaMoedas[i];
+									t1.QtdMoedas[i] = t1.QtdMoedas[i] - 1;
+									t1.RetornaMoedas[i] = t1.RetornaMoedas[i] + 1;
+						  		}
+							}
+						}
+						for (int j = 0; j < t1.arrSize; j++) {
+							if (t1.RetornaMoedas[j] != 0){
+								cout << "Devolvendo " << t1.RetornaMoedas[j] << " moedas de " << t1.ListaMoedas[j] << " euros!\n";	
+							}
+							t1.RetornaMoedas[j] == 0;
+						}
+					}
+					p[escolha].setquantidade(p[escolha].getquantidade() - 1);
+				}
+			}
+		}	
+	}	
 }
 
 main(){
-	int qtd;
 	cout << "Insira a quantidade de produtos a inserir: ";
 	cin >> qtd;
-	stock(qtd);
-	
-	troco();
+	stock(qtd,0);
 }
